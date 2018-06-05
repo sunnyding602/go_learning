823e4a2990a74f55677c22b01602ca0f30fdf75c
Runxi Ding <runxi.ding@streamlabs.com>
2018-06-05 00:14:20 -0700

    02 packages the comments for exported function and var are really helpful

---
+++ 02_packages/main/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/sunnyding602/go_learning/02_packages/icomefromalaska"
+	"github.com/sunnyding602/go_learning/02_packages/string_util"
+)
+
+func main() {
+	fmt.Println(stringutil.Reverse("!oG ,olleH"))
+	fmt.Println(stringutil.MyName)
+	fmt.Println(winniepooh.BearName)
+}
